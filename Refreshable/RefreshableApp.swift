d75e76f9ccc6a75f9852adda088e6f1fe181585f
Niklas Hein <niklas.hein@simnik.net>
2022-10-05 14:29:57 +0200

    Initial Commit

---
+++ Refreshable/RefreshableApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefreshableApp.swift
+//  Refreshable
+//
+//  Created by Niklas Hein on 05.10.22.
+//
+
+import SwiftUI
+
+@main
+struct RefreshableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
